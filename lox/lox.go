d6e75f8744355023ec08c4b6631245898bc04435
Ben Muthalaly <benmuthalaly@gmail.com>
2024-07-10 23:54:35 -0500

    Split off lox interpreter to separate package.
    Also added error handling

---
+++ lox/lox.go
@@ -0,0 +1,65 @@
+package lox
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Interpreter struct {
+	hadError bool
+}
+
+// RunFile runs a glox script in a file
+func (itpr *Interpreter) RunFile(fileName string) error {
+	fmt.Println(fileName)
+	content, err := os.ReadFile(fileName)
+	if err != nil {
+		return err
+	}
+	itpr.Run(content)
+	return nil
+}
+
+// RunPrompt runs glox interactively
+func (itpr *Interpreter) RunPrompt() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("> ")
+		scanner.Scan()
+		line := scanner.Bytes()
+		if len(line) == 0 {
+			break
+		}
+		itpr.Run(line)
+	}
+}
+
+// Run actually runs the interpreter
+func (itpr *Interpreter) Run(content []byte) {
+	tokens := bytes.Split(content, []byte{' ', '\n', '\t', '\r'})
+
+	for _, token := range tokens {
+		fmt.Println(string(token))
+	}
+}
+
+// error reports syntax errors
+func (itpr *Interpreter) error(line int, message string) error {
+	if err := itpr.report(line, "", message); err != nil {
+		return err
+	}
+	return nil
+}
+
+// report writes an error message to stderr
+func (itpr *Interpreter) report(line int, where, message string) error {
+	_, err := io.WriteString(os.Stderr, fmt.Sprintf("[line %d] Error %s: %s", line, where, message))
+	if err != nil {
+		return err
+	}
+	itpr.hadError = true
+	return nil
+}
--- main.go
+++ main.go
@@ -1,58 +1,24 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
+	"github.com/benmuth/crafting-interpreters/glox/lox"
 	"os"
 )
 
 func main() {
 	args := os.Args[1:]
 
+	itpr := new(lox.Interpreter)
 	if len(args) > 1 {
 		fmt.Println("Usage: jlox [script]")
 		os.Exit(64)
 	} else if len(args) == 1 {
-		err := runFile(args[0])
+		err := itpr.RunFile(args[0])
 		if err != nil {
 			fmt.Println("Couldn't read script contents")
 		}
 	} else {
-		runPrompt()
-	}
-}
-
-// runFile runs a glox script in a file
-func runFile(fileName string) error {
-	fmt.Println(fileName)
-	content, err := os.ReadFile(fileName)
-	if err != nil {
-		return err
-	}
-	run(content)
-	return nil
-}
-
-// runPrompt runs glox interactively
-func runPrompt() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Printf("> ")
-		scanner.Scan()
-		line := scanner.Bytes()
-		if len(line) == 0 {
-			break
-		}
-		run(line)
-	}
-}
-
-// run actually runs the interpreter
-func run(content []byte) {
-	tokens := bytes.Split(content, []byte{' ', '\n', '\t', '\r'})
-
-	for _, token := range tokens {
-		fmt.Println(string(token))
+		itpr.RunPrompt()
 	}
 }
